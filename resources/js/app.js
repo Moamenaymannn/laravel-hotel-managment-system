9862c7f9d77ca280d64ee74b7109435e88e50384
moamen ayman <moamenayman596@gmail.com>
2025-12-09 18:21:37 +0200

    Fix Ziggy routes to use dynamic @routes directive

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,6 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
-import { Ziggy } from './ziggy';
 import Toast from "vue-toastification"
 import "vue-toastification/dist/index.css"
 
@@ -29,7 +28,7 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     return createApp({ render: () => h(App, props) })
       .use(plugin)
-      .use(ZiggyVue, Ziggy)
+      .use(ZiggyVue)
       .use(Toast)
       .mount(el);
   },
